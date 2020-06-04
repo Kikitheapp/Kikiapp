699465cce7a9421c5c14a5d3b201adc148b80473
nick Frattaroli <nafratt@gmail.com>
2020-06-04 15:17:02 -0400

    setting up UI

--- sexyedu-app/src/App.jsx
+++ sexyedu-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React  
         </a>
         <Logo></Logo>
         <Survey></Survey>
--- sexyedu-app/src/components/mailinglist.jsx
+++ sexyedu-app/src/components/mailinglist.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Mailing(props) {
     return (
         <div className="mailinglist">
-            <a href="www.google.com">join our mailing list</a>
+            <a href="test.html">join our mailing list</a>
         </div>
     )
 }
