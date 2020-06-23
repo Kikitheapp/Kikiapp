bdf8bfec87d0a1adb682a6aa904b7b3ecbdc8ab0
nick Frattaroli <nafratt@gmail.com>
2020-06-23 12:41:53 -0400

    font awesome

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Footer(props) {
-    return (
-        <footer>
-        <div className="container">
-          <span>Sexuali-TEA</span>
-          {/* social icons 
-          Facebook
-          Twitter
-          Instagram
-           */}
-        </div>
-      </footer>
-    )
+  return (
+    <footer>
+      <div className="container">
+        <span>Sexuali-TEA</span>
+        <FontAwesomeIcon icon={faFacebook}
+          className="socialIcon"
+          href="https://www.facebook.com/" />
+        <FontAwesomeIcon icon={faInstagram} className="socialIcon"
+          href="https://www.instagram.com/" />
+        <FontAwesomeIcon icon={faTwitter} className="socialIcon"
+          href="https://twitter.com" target="_blank"/>
+      </div>
+    </footer>
+  )
 }
