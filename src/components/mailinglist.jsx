455dd1dd99f770344286601e7d6af9eebc1a5121
nick Frattaroli <nafratt@gmail.com>
2020-08-20 15:45:51 -0400

    form prep fixed favicon broken blog link

--- src/components/mailinglist.jsx
+++ src/components/mailinglist.jsx
@@ -4,12 +4,9 @@ export default function Mailing(props) {
     return (
         <div className="mailinglist">
             <p className="slogan">Get a fresh take on what you didn't learn in sex-ed.</p>
-            <form><input className="emailinput"placeholder="jwhite@gmail.com"></input></form>
-            <button className="mailinglistbtn" href="test.html">Join our mailing list</button>
-            <div className="survey">
+            <a  href="form.html"><button className="mailinglistbtn">Join our mailing list</button></a>
             <a className="quizlink" href="https://kristawhite122148.typeform.com/to/hNxxl3 " target="_blank" rel="noopener noreferrer">or take this short quiz</a>
             
-        </div>
         </div>
     )
 }
