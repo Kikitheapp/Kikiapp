8c3326d4955aefa795a286e566be304f93576873
nick Frattaroli <nafratt@gmail.com>
2020-09-08 10:35:40 -0400

    adding form css and maillist edits

--- src/components/mailinglist.jsx
+++ src/components/mailinglist.jsx
@@ -5,7 +5,7 @@ export default function Mailing(props) {
         <div className="mailinglist">
             <p className="slogan">Get a fresh take on what you didn't learn in sex-ed.</p>
             <a  href="form.html"><button className="mailinglistbtn">Join our mailing list</button></a>
-            <a className="quizlink" href="https://kristawhite122148.typeform.com/to/hNxxl3 " target="_blank" rel="noopener noreferrer">or take this short quiz</a>
+            <a className="quizlink" href="https://kristawhite122148.typeform.com/to/hNxxl3 " target="_blank" rel="noopener noreferrer">or take a quick Survey</a>
             
         </div>
     )
