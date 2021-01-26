92a07ae6c4cb705e749b8130e8e92db8e11c3c54
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2021-01-26 13:09:37 -0500

    react-router-dom, links, and aboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,302 @@
+import React from 'react'
+import '../AboutUs.css'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div className='SectionContainer'>
+        <p>
+          We are Kiki. We are creating an app that curates sexual health
+          resources for the queer community. We are a platform for queer sexual
+          education, for sexual liberation, for knowledge, for power, joy,
+          freedom, and connection. Stay tuned for the launch of our app. For
+          now, check out our blog, sign up for our newsletter, and follow us on
+          social media.
+        </p>
+        <p>
+          Kiki was founded in 2020 to address the gap in sexual health resources
+          and education for the LGBTQIA+ community. This lack of education
+          starts early: one study found that fewer than 5% of LGBT youth have
+          access to positive representations of LGBT sexuality in their sex ed
+          classes. This renders us essentially invisible and creates a barrier
+          to having safe, pleasurable sex lives. We also face healthcare
+          disparities like doctors who are not educated in queer sexual health
+          needs that lead to higher rates of STIs and fewer preventative
+          healthcare visits. Learning about sexual health and pleasure shouldn’t
+          be daunting. We created Kiki as a way to counter the cisgender,
+          heterosexual-centered sex ed that is taught in most schools in the US.
+          This platform aims to create space for fun, fact-based, and
+          easy-to-understand discussions about sex. For the newly out to the old
+          pros, for everything from basic anatomy to butt plugs, Kiki is about
+          inclusion for all. All genders, all sexualities, all types of sex and
+          relationships.
+        </p>
+      </div>
+      <div>
+        {'\n'}
+        <div className='block'></div>
+        <div>
+          <h1 class='header-1'>Meet Our Badass Team</h1>
+          {'\n'}
+          {/* <!-- Bios for the team --> */}
+          <div className='container'>
+            <div className='Bio grow'>
+              <h4>Krista White (She, Her, Hers)</h4>
+              <img
+                className='bio-img'
+                src='./Assets/images/Krista.png'
+                alt='Krista White (Founder)'
+              ></img>
+              {'\n'}
+              <h3>Founder</h3>
+              <div className='content'>
+                <p>
+                  Krista White is a queer, Black writer and actor who decided to
+                  take a UX Design class on a whim in 2019. She fell in love
+                  with her final project, a prototype for the sexual health app
+                  that would become Kiki. She is thrilled to be working with an
+                  incredible team to bring this work to fruition. Based in New
+                  York City, she loves sci-fi, baking, and writing fiction.
+                </p>
+                {'\n'}
+                {'\n'}
+                Blog:
+                <a
+                  id='about-links'
+                  href='https://www.aroundtheworldin80plays.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {' '}
+                  aroundtheworldin80plays.com
+                </a>
+                {'\n'}
+                Twitter:
+                <a
+                  id='about-links'
+                  href='https://www.twitter.com/thekristawhite'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  @thekristawhite{' '}
+                </a>
+                {'\n'}
+                Instagram:
+                <a
+                  id='about-links'
+                  href='https:www.instagram.com/thekristawhite/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  @thekristawhite{' '}
+                </a>
+                {'\n'}
+                LinkedIn:
+                <a
+                  id='about-links'
+                  href='https:www.linkedin.com/in/kristawhite3'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  kristawhite3
+                </a>
+              </div>
+            </div>
+            <div className='Bio grow'>
+              <h4>Shanice Bailey (She, Her, Hers)</h4>
+              <img
+                className='bio-img'
+                src='./Assets/images/Shanice.jpg'
+                alt='Shanice Bailey (Intern)'
+              ></img>
+              {'\n'}
+              <h3>Writer and Researcher</h3>
+              <div className='content'>
+                <p>
+                  Shanice is a black queer curator and cultural studies scholar
+                  based in the NYC area. As someone who spent her formative
+                  years covertly scrolling through gURL.com, she's excited to
+                  help build this new sex-positive digital space. She's
+                  passionate about queer history, internet culture, camp, filth,
+                  and Dolly Parton, her toothless chihuahua.
+                </p>
+                {'\n'}
+                {'\n'}
+                Instagram:
+                <a
+                  id='about-links'
+                  href='https:www.instagram.com/uvul.a/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  @uvul.a{' '}
+                </a>
+              </div>
+            </div>
+            <div className='Bio grow'>
+              <h4>Nicholas Frattaroli (He, Him, His)</h4>
+              <img
+                className='bio-img'
+                src='./Assets/images/Nick.jpg'
+                alt='Nick Frattaroli (Web Dev)'
+              ></img>
+              {'\n'}
+              <h3>Web Developer</h3>
+              <div className='content'>
+                <p>
+                  Nick is a Web Developer based out of NYC. As a proud member of
+                  the Bear community He is excited to help bring sexual health
+                  and education to underserved members of the LGBTQ+ community.
+                  An avid fan of all things Nerdy(From ESports to D&D) he is
+                  excited to help others by aiding in the creation of a digital
+                  format to disseminate helpful information in one convenient
+                  place.
+                  {'\n'}
+                </p>
+                {'\n'}
+                LinkedIn:
+                <a
+                  id='about-links'
+                  href='https:www.linkedin.com/in/nicholas-frattaroli'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  nicholas-frattaroli{' '}
+                </a>
+                {'\n'}
+                Github:
+                <a
+                  id='about-links'
+                  href='https:github.com/Nfratt'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github.com/Nfratt
+                </a>
+              </div>
+            </div>
+            <div className='Bio grow'>
+              <h4>Kate Bader (She,Her,Hers)</h4>{' '}
+              <img
+                className='bio-img'
+                src='./Assets/images/Kate.jpg'
+                alt='Kate Bader(Social Media Manager)'
+              ></img>
+              {'\n'}
+              <h3>Social Media Manager</h3>{' '}
+              <div className='content'>
+                <p>
+                  Kate Bader is a NYC based actor and content creator, with a
+                  background in film production, marketing, and healthcare. She
+                  adores the way social media bridges community engagement,
+                  visual aesthetics, entertainment, and digestible knowledge.
+                  She is excited to be working on this team of talented
+                  individuals, and helping to bring curated sexual health info
+                  to a community she loves. {'\n'}
+                </p>
+                {'\n'}
+                Instagram:{' '}
+                <a
+                  id='about-links'
+                  href='https://www.instagram.com/kate_bader_/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  @Kate_bader_{' '}
+                </a>{' '}
+              </div>{' '}
+            </div>{' '}
+            <div className='Bio grow'>
+              <h4>Mick Franco (he/him/his)</h4>{' '}
+              <img
+                className='bio-img'
+                src='./Assets/images/mick.jpg'
+                alt='Mick Franco (Data Scientist)'
+              ></img>
+              {'\n'}
+              <h3>Data Scientist</h3>{' '}
+              <div className='content'>
+                <p>
+                  Mick is a Data Scientist based in Connecticut. As a proud
+                  member of the LGBTQ community, he excited to contribute his
+                  analytical skills to helping the team identify underserved
+                  groups and the sexual health information that they need but
+                  might not be able to find elsewhere. He always likes to stay
+                  abreast of current events, but is also passionate about
+                  cooking, gardening, and hockey (go Bruins!). {'\n'}
+                </p>
+                {'\n'}
+                LinkedIn:{' '}
+                <a
+                  id='about-links'
+                  href='https://www.linkedin.com/in/michael-j-franco-jr/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  michael-j-franco-jr{' '}
+                </a>{' '}
+              </div>{' '}
+            </div>{' '}
+            <div className='Bio grow'>
+              <h4>Fiona Mustain (She, Her, Hers) </h4>{' '}
+              <img
+                className='bio-img'
+                src='./Assets/images/Mustain.Fiona.Middlebury.03.JPG'
+                alt='Fiona Mustain (Ux Designer)'
+              ></img>
+              {'\n'}
+              <h3>UX Designer</h3>
+              <div className='content'>
+                <p>
+                  Fiona is a UX designer based in Austin, Texas with a
+                  background in Architecture and Political Science. As a
+                  designer, her goal is to create a positive impact. She is
+                  excited to help design and build a positive space to bring
+                  sexual health to the LGBTG+ community. Apart from being on
+                  Figma, she is passionate about traveling, cooking, and
+                  reading.
+                  {'\n'}
+                </p>
+                {'\n'}
+              </div>
+            </div>
+            <div className='Bio grow'>
+              <h4>Jesse Green (They/Them)</h4>
+              <img
+                className='bio-img'
+                src='./Assets/images/Jesse.jpg'
+                alt='Jesse Green(Web Developer)'
+              ></img>
+              {'\n'}
+              <h3>Web Developer</h3>
+              <div className='content'>
+                <p>
+                  Jesse is a Web Developer based out of Salt lake city, Utah. As
+                  part of the LGBTQ community, They are excited to help build a
+                  safe sex education to contribrute to the members around them.
+                  Also shining positivity in queer spaces that sometimes get
+                  look over. They love to bake, paint, and go hiking with their
+                  goldendoodle. You can also catch them at a local coffeeshop
+                  drinking a latte if your lucky.
+                  {'\n'}
+                </p>
+                {'\n'}
+                Instagram:{' '}
+                <a
+                  id='about-links'
+                  target='_blank'
+                  rel=''
+                  href='https://www.instagram.com/queer_moonchild/'
+                >
+                  @queer_moonchild
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,19 +1,28 @@
-import React from "react";
-
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
-    return (
-        <div className="navbar">
-            <ul>
-                <li>
-                    <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@kikiapp">
-                        <p>Visit our blog</p></a>
-                </li>
-                <li>
-                    <a href="./aboutus.html" >
-                        <p>About us</p></a>
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className='navbar'>
+      <ul>
+        <li>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://medium.com/@kikiapp'
+          >
+            <p>Visit our blog</p>
+          </a>
+        </li>
+        <li>
+          <Link to='/aboutUs'>
+            <p>About us</p>
+          </Link>
+
+          {/* needs path to the form component <Link to='/'>
+          <p>Join Our Mailing List</p></Link> */}
+        </li>
+      </ul>
+    </div>
+  )
 }
