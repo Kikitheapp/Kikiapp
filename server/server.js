63a8b4123daf4db05fb904be2c685a3ef5688a02
Amanda Castonguay <alcastonguay@gmail.com>
2021-01-29 20:12:25 -0500

    Added scripts to package.json to start server for development and launch app from server for production.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/hey', (req, res) => res.send('ho!'));
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/ping', (req, res) => {
+    return res.send('pong');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 
 app.listen(8080);
