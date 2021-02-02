1de20fcec26f48bed936a9c45874501a222b2ab2
Nick Fratt <nafratt@gmail.com>
2021-02-02 15:02:03 -0500

    routeing for form

--- src/components/mailinglist.jsx
+++ src/components/mailinglist.jsx
@@ -7,7 +7,7 @@ export default function Mailing(props) {
             <a href="./form.html"><button className="mailinglistbtn">Join our mailing list</button></a>
             <br></br>
             <a className="quizlink" href="https://kristawhite122148.typeform.com/to/hNxxl3 " target="_blank" rel="noopener noreferrer">...Or take a quick survey</a>
-            
+
         </div>
     )
 }
