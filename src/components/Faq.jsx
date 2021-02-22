798ad13e62cc1aab51683a56ef3a0c432f7fff70
Amanda Castonguay <alcastonguay@gmail.com>
2021-02-21 20:56:38 -0500

    Added basic structure to add the FAQ page as a react component.

---
+++ src/components/Faq.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../AboutUs.css'
+
+const Faq = () => {
+    return (
+        <div>
+            <div className='SectionContainer'>Hello There</div>
+        </div>
+    )
+}
+
+export default Faq
