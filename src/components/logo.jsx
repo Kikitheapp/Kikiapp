2a8cea433b18ebe72b93e8cef800121905040f9f
Chiara Marcial Martínez <contact@cofuente.io>
2021-03-15 14:30:46 -0400

    add code comment
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// Should reconsider how we organize these assets. Why are there two assets folders?
 import Kikilogo from '../assets/images/kikilogo1.png'
 import { Link } from 'react-router-dom'
 export default function Logo(props) {
