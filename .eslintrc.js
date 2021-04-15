348d2744ab113725d1cca260216393422a932e0e
Nick Fratt <nafratt@gmail.com>
2021-04-15 14:51:56 -0400

    specify react version

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,13 @@ module.exports = {
         "react"
     ],
     "rules": {
+    },
+    "settings":{
+        "react":{
+            "version": "detect" // React version. "detect" automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // default to latest and warns if missing
+                           // It will default to "detect" in the future
+        }
     }
 };
