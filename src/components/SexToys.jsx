5b871b50aea2cc997cfb78ee33a1c1b08dffb44b
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2021-05-03 14:59:38 -0400

    rendering data on the page

--- src/components/SexToys.jsx
+++ src/components/SexToys.jsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from 'react'
 import { baseURL, config } from '../services'
 
 const SexToys = () => {
-  const [toys, setToys] = useState('')
+  const [toys, setToys] = useState([])
 
   useEffect(() => {
     const getToys = async () => {
       const response = await axios.get(baseURL, config)
+      console.log(response)
       setToys(response.data.records)
     }
     getToys()
@@ -17,20 +18,15 @@ const SexToys = () => {
   return (
     <div>
       {toys.map((toy) => {
-        return <div></div>
+        return (
+          <div>
+            <img src={toy.fields.Image} />
+            <h1>{toy.fields.Name}</h1>
+          </div>
+        )
       })}
     </div>
   )
 }
-
-return (
-  <>
-    {facts && (
-      <div>
-        <h3>"{facts[Math.floor(Math.random() * facts.length)].fields.fact}"</h3>
-      </div>
-    )}
-  </>
-)
 
 export default SexToys
