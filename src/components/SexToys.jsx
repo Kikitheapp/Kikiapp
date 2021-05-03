a0922289bc9181535a7de1ffe0c3cc83f484d74f
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2021-05-03 11:34:17 -0400

    added a useeffect to get data

--- src/components/SexToys.jsx
+++ src/components/SexToys.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { baseURL, config } from '../services'
 
 const SexToys = () => {
+  const [toys, setToys] = useState('')
+
+  useEffect(() => {
+    const getToys = async () => {
+      const response = await axios.get(baseURL, config)
+      setToys(response.data.records)
+    }
+    getToys()
+  }, [])
+
   return (
     <div>
-      <h1>test</h1>
+      {toys.map((toy) => {
+        return <div></div>
+      })}
     </div>
   )
 }
+
+return (
+  <>
+    {facts && (
+      <div>
+        <h3>"{facts[Math.floor(Math.random() * facts.length)].fields.fact}"</h3>
+      </div>
+    )}
+  </>
+)
 
 export default SexToys
