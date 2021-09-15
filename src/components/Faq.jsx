2c0e9a14a709808cb1168befe66dc15f4da15eff
Nick Fratt <nafratt@gmail.com>
2021-09-15 11:41:03 -0400

    fixing  darkmode coloring for prep page

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import person1 from "../assets/images/faq-person-1.svg";
 import person2 from "../assets/images/faq-person-2.svg";
+import person3 from "../assets/images/prep-person-3.svg";
+import person4 from "../assets/images/prep-person-4.svg";
+import person5 from "../assets/images/prep-person-5.svg";
+
 import Accordion from './accordion';
 const FAQ = () => {
     return (
@@ -8,71 +12,70 @@ const FAQ = () => {
             <h2 className="text-center bg-peach-puff">FAQ</h2>
             <div className="prep-people row justify-content-center mb-5">
                 <div className="prep-person-1 col-6 text-right">
-                    <img src={person1} className="img-fluid"
+                    <img src={person2} className="img-fluid"
                         alt="Person with white shirt and blue skirt walking to meet person
                     on right." />
                 </div>
                 <div className="prep-person-2 col-6">
-                    <img src={person2} className="img-fluid"
+                    <img src={person1} className="img-fluid"
                         alt="Person with orange jacket and blue jeans walking to meet person 
                     on left."/>
                 </div>
             </div>
         <div className="wrapper">
-            <Accordion title=" STIs/STDs, Testing, and Barriers">
-                <a href='https://medium.com/kiki-app/hiv-aids-101-5ede46dd1394'target='_blank'rel='noopener noreferrer'>
-                HIV/AIDs 101
-                </a>
-                <p>The first STI/STD on many people’s minds is HIV/AIDS. Our HIV/AIDS 101 Guide helps break it down.</p>
-                <a href='https://medium.com/kiki-app/how-to-get-tested-for-stis-35938800f54' target='_blank'rel='noopener noreferrer' >
-                How To Get Tested
-                </a>
-                <p>The lowdown on where, how, and when to get tested for STIs.</p>
-            </Accordion>
-            <Accordion title="Who Should Start PrEP?">
-                <p>PrEP (specifically Truvada) is highly effective (more than 90%)  in preventing HIV transmission from sex for people of all 
-                genders and sexualities. People who might take PrEP include folks with a sexual partner who is HIV positive, folks with a partner 
-                whose HIV status is unknown, people who have tested positive for an STI in the past 6 months (which puts them at higher risk for 
-                contracting HIV), and people who inject non-prescription drugs. Chat with your doctor if you think PrEP might be right for you.</p>
-
-            </Accordion>
-            <Accordion title="Where To Get It?">
-                <p>PrEP is covered under most private and public health insurance. Check out this directory for PrEP providers near you. 
-                It is also available through telehealth companies like Folx</p>
-
-            </Accordion>
-            <Accordion title="Other Things To Know">
+            <Accordion title="STIs/STDs, Testing, and Barriers">
                 <ul>
-                    <li>  It’s important that you take your PrEP pills every day. If you skip pills, there may not be enough of the medication in your system to block 
-                        transmission of HIV.</li>
-                    <li>  Since PrEP is just for HIV prevention, you still need to take precautions like barrier methods and regular testing to prevent other STIs/STDs.</li>
-                    <li> Side effects like nausea, loss of appetite, and headaches may occur, but usually go away over time</li>
-                    <li> Once you’re on PrEP, you’ll need to visit your doctor once every three months for follow-ups on how the medication is working and an HIV test. 
-                        This may take the form of an in-person visit or a home test and telemedicine visit.</li>
-                    <li>  If you’ve been exposed to HIV and aren’t on PrEP, you can take PEP (post-exposure prophylaxis) within 72 hours of exposure. The sooner you can get
-                        PEP, the more effective it is, and it must be taken daily for 28 days.</li>
+                    <li>
+                        <a href='https://medium.com/kiki-app/hiv-aids-101-5ede46dd1394'target='_blank'rel='noopener noreferrer'>
+                            <p>HIV/AIDs 101</p>
+                        </a>
+                    </li>
+                    <p>The first STI/STD on many people’s minds is HIV/AIDS. Our HIV/AIDS 101 Guide helps break it down.</p>
+                    <li>
+                        <a href='https://medium.com/kiki-app/how-to-get-tested-for-stis-35938800f54' target='_blank'rel='noopener noreferrer' >
+                            <p>How To Get Tested</p>
+                        </a>
+                </li>
+                    <p>The lowdown on where, how, and when to get tested for STIs.</p>
                 </ul>
+                <img src={person3} className="img-fluid"alt="Person with jean jacket and jeans giving another person with purple pants and a red jacket a piggy back ride." />
             </Accordion>
-            <Accordion title="Sources">
-                <div className="sources-links">
-                    <ul>
-                        <li>CDC. (2020, November 3). About Prep. HIV Basics.<a href="https://www.cdc.gov/hiv/basics/prep/about-prep.html" target="_blank" rel="noopener noreferrer">
-                            https://www.cdc.gov/hiv/basics/prep/about-prep.html</a>
-                        </li>
-                        <li>Planned Parenthood. (n.d.). What Is PrEP. Planned Parenthood. 
-                            <a href="https://www.plannedparenthood.org/learn/stds-hiv-safer-sex/hiv-aids/prep" target="_blank" rel="noopener noreferrer">
-                            https://www.plannedparenthood.org/learn/stds-hiv-safer-sex/hiv-aids/prep </a>
-                        </li>
-                        <li>Please Prep Me. (2021, January 15). Please Prep Me. Please Prep Me. <a href="https://pleaseprepme.org/"target="_blank" rel="noopener noreferrer">
-                            https://pleaseprepme.org/ </a>
-                        </li>
-                    </ul>
-                </div>
+            {/* <Accordion title="Sexual Pleasure"> */
+          /* This link is currently returning a 404 keep out for now */
+            /* <a a href='https://kikiapp.medium.com/intro-to-anal-sex-fd609bc73442https://kikiapp.medium.com/intro-to-anal-sex-fd609bc73442'target='_blank'rel='noopener noreferrer'>
+            /* <p>A beginner’s guide to butt stuff.</p>
+            /* <img src={person3} className="img-fluid"alt="Person with jean jacket and jeans giving another person with purple pants and a red jacket a piggy back ride." />
+            /* </Accordion> */}
+            <Accordion title="Reproductive Health">
+                <ul>
+                    <li>
+                    <a href='https://kikiapp.medium.com/what-you-need-to-know-about-birth-control-6ea439bdef2b' target='_blank'rel='noopener noreferrer'>
+                        <p>What You Need To Know About Birth Control</p>
+                    </a>
+                    <p>A gender neutral guide to birth control basics.</p>
+                    </li>
+                </ul>
+                <img src={person4} className="img-fluid"
+                    alt="Person with jean jacket and jeans giving another person with purple pants and a red jacket a piggy back ride." />
+            </Accordion>
+            <Accordion title="Relationships and Consent">
+                <ul>
+                    <li>
+                        <a href='https://kikiapp.medium.com/what-you-need-to-know-about-birth-control-6ea439bdef2b' target='_blank'rel='noopener noreferrer'>
+                            <p>Consent Is Mandatory</p>
+                        </a>
+                        <p>
+                        The basics of sexual consent.
+                        </p>
+                    </li>
+                </ul>
+                <img src={person5} className="img-fluid"
+                    alt="Person with jean jacket and jeans giving another person with purple pants and a red jacket a piggy back ride." />
             </Accordion>
 
-        </div>
-        </div>
-    )
+            </div>
+            </div>
+    )   
 }
 
 export default FAQ;
