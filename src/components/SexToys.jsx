929652c00c7c783d478850cc61feaa395375548b
Amanda Castonguay <alcastonguay@gmail.com>
2021-09-25 10:47:36 -0400

    Added Bootstrap Card to display sex toy information. Add placehold image, needs to be replaced with real images.

--- src/components/SexToys.jsx
+++ src/components/SexToys.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Heading from './Heading'
-import person1 from "../assets/images/prep-person-1.svg";
-import person2 from "../assets/images/prep-person-2.svg";
+import person1 from "../assets/images/sextoys-person-1.svg";
+import person2 from "../assets/images/sextoys-person-2.svg";
+import placeholder from "../assets/images/sextoys-placeholder.jpg";
 
 const SexToys = () => {
     const pageInfo = {
@@ -11,11 +12,94 @@ const SexToys = () => {
         person2: person2
     }
 
+    const sexToys = [
+        {
+            name: 'Dildo',
+            emoji: '🌶',
+            starSign: 'Aries',
+            bio: 'Hey babe, I can’t wait to be inside you. Use me alone or pop me into a strap-on \
+                for hands-free partner play. Find me in flesh tones or in every color of the \
+                rainbow if that "realistic dick" look isn’t for you. '
+        },
+        {
+            name: 'Vibrator',
+            emoji: '🔥',
+            starSign: 'Gemini',
+            bio: 'Good 👏🏾 vibes 👏🏾 only. Perhaps the most truly vers of all my \
+                pals, I can be used by anyone, all over your body. Sometimes I’m feeling cute and \
+                casual, as a discreet little battery-powered lipstick vibe, and sometimes I’m \
+                ready to rumble as the classic magic wand. Rub a dub dub, get a waterproof \
+                version for some fun in the tub.'
+        },
+        {
+            name: 'Butt Plug',
+            emoji: '🍑',
+            starSign: 'Sagittarius',
+            bio: 'Booty booty booty booty rocking everywhere. I fancy myself as being rather \
+            inclusive – everyone’s got a butt, after all. I come in many sizes depending on how \
+            thiccc you like your penetration and how much anal experience you have. Find me in \
+            low tech silicone models or vibrating versions to really get your gears going. \
+            Remember, I’ve gotta have a flared base, or I might get lost inside you. '
+        },
+        {
+            name: 'Cock Ring',
+            emoji: '🍆',
+            starSign: 'Virgo',
+            bio: 'Will you be my main squeeze? I up the pleasure for people with penises by \
+            increasing the blood flow, which can make for stronger erections and more intense \
+            orgasms. Need I say more? Oh and if you want, I can vibrate, which can be a lot of \
+            fun for both you and your partner. '
+        },
+        {
+            name: 'Kitty Whip',
+            emoji: '😈',
+            starSign: 'Scorpio',
+            bio: 'I may be bad but I’m perfectly good at it. I’m a great intro for those new to \
+            BDSM and am known for adding a little power and pain play into your regularly \
+            scheduled programming. I’m good for caressing, tickling, and of course, a good hard \
+            smack on the ass. Use me with my friends blindfold and handcuffs to turn things up a \
+            notch. '
+        },
+        {
+            name: 'Lube',
+            emoji: '💦',
+            starSign: 'Cancer',
+            bio: 'Slippery when wet. I’m best friends with everyone because I like to let things \
+            slide. I come in flavors, in water or aloe based (for skin-to-skin, condoms, and \
+            toys), long-lasting and hypoallergenic silicone-based (for skin-to-skin and condoms) \
+            or oil-based (for skin-to-skin). '
+            
+        }
+    ]
+
+    let buildSexToyDivs = (sexToy) => {  
+        return (
+            <div key={sexToy.name} className="col py-3 align-item-stretch">
+                <div className="card bg-light-gray border-0 rounded-lg">
+                    <img src={placeholder} className="card-img-top rounded-top" />
+                    <div className="card-body">
+                        <h3 className="card-title" style={{textDecoration: "underline" }}>{sexToy.name}</h3>
+                        <h4 className="">Fave Emoji: {sexToy.emoji}</h4>
+                        <h4 className="">Star Sign: {sexToy.starSign}</h4>
+                        <p className="card-text">
+                            {sexToy.bio}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    let sexToyCols = sexToys.map(buildSexToyDivs);
+
     return (
-        <div>
+        <div className="sex-toys">
             <Heading info={pageInfo}></Heading>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 px-5">
+                {sexToyCols} 
+            </div>
         </div>
         )
 }
 
-export default SexToys;
+export default SexToys; 
