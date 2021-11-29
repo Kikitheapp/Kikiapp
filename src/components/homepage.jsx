cd27f1b3b9f3321b4ffd0a896477e07bc0ba5086
Amanda Castonguay <alcastonguay@gmail.com>
2021-11-29 14:31:40 -0500

    Update URL for RSS feed.

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -17,7 +17,7 @@ export default function homepage() {
     async function getFeed() {
         try{
             let feedInfo = [];
-            let thisFeed = await parser.parseURL('/feed/@kikiapp');
+            let thisFeed = await parser.parseURL('https://medium.com/feed/@kikiapp');
             for(let i=0; i<3; i++){
                 let item = thisFeed.items[i];
                 let story = {
