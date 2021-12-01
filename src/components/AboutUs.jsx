9682c70f7f1ce6c7740e9f867dba0677af03068c
Nick Fratt <nafratt@gmail.com>
2021-12-01 11:59:40 -0500

    new about copy

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,30 +6,33 @@ const AboutUs = () => {
     <div>
       <div className='SectionContainer'>
         <p>
-          We are Kiki. We are creating an app that curates sexual health
-          resources for the queer community. We are a platform for queer sexual
-          education, for sexual liberation, for knowledge, for power, joy,
-          freedom, and connection. Stay tuned for the launch of our app. For
-          now, check out our blog, sign up for our newsletter, and follow us on
-          social media.
+        We are Kiki For The Future™. We are creating an app that curates sexual health resources for the queer community. 
+        We are a platform for queer sexual education, for sexual liberation, for knowledge, for power, joy, freedom, and connection. 
+        Stay tuned for the launch of our app. For now, check out our
+        <a target='_blank' href='https://medium.com/kiki-app' rel="noreferrer"> blog </a>, 
+        sign up for our
+        <a target='_blank' href='https://kikitheapp.com/#/MailinglistForm' rel="noreferrer"> newsletter</a>, 
+        and follow us on
+        <a target='_blank' href='https://www.instagram.com/kikitheapp/' rel="noreferrer"> Instagram,</a>
+        <a target='_blank' href='https://www.facebook.com/kikitheapp' rel="noreferrer"> Facebook,</a>
+        <a target='_blank' href='https://twitter.com/kikitheapp' rel="noreferrer"> Twitter,</a>
+        and 
+        <a target='_blank' href='https://www.tiktok.com/@kikitheapp?' rel="noreferrer"> TikTok. </a>
+        You can also join us on Patreon for exclusive content.
         </p>
         <p>
-          Kiki was founded in 2020 to address the gap in sexual health resources
-          and education for the LGBTQIA+ community. This lack of education
-          starts early: one study found that fewer than 5% of LGBT youth have
-          access to positive representations of LGBT sexuality in their sex ed
-          classes. This renders us essentially invisible and creates a barrier
-          to having safe, pleasurable sex lives. We also face healthcare
-          disparities like doctors who are not educated in queer sexual health
-          needs that lead to higher rates of STIs and fewer preventative
-          healthcare visits. Learning about sexual health and pleasure shouldn’t
-          be daunting. We created Kiki as a way to counter the cisgender,
-          heterosexual-centered sex ed that is taught in most schools in the US.
-          This platform aims to create space for fun, fact-based, and
-          easy-to-understand discussions about sex. For the newly out to the old
-          pros, for everything from basic anatomy to butt plugs, Kiki is about
-          inclusion for all. All genders, all sexualities, all types of sex and
-          relationships.
+        Kiki For The Future™ was founded in 2020 to address the gap in sexual health resources and education for the LGBTQIA+ community. 
+        This lack of education starts early:
+        <a target='_blank' href='https://www.glsen.org/research/2019-national-school-climate-survey' rel="noreferrer"> one study </a>
+         found that fewer than 8% of LGBT youth have access to positive representations 
+        of LGBT sexuality in their sex ed classes. This renders us essentially invisible and creates a barrier to having safe, 
+        pleasurable sex lives. We also face healthcare disparities like doctors who are not educated in queer sexual health needs 
+        that lead to higher rates of STIs and fewer preventative healthcare visits. Learning about sexual health and pleasure shouldn’t be daunting.
+        </p>
+        <p>
+        We created Kiki For The Future™ as a way to counter the cisgender, heterosexual-centered sex ed that is taught in most schools in the US.
+         This platform aims to create space for fun, fact-based, and easy-to-understand discussions about sex. For the newly out to the old pros,
+         for everything from basic anatomy to butt plugs, Kiki For The Future™ is about inclusion for all. All genders, all sexualities, all types of sex and relationships.
         </p>
       </div>
       <div>
