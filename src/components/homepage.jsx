d231a9bba25a1afcd93e48d37b892ff6d98d45fe
Amanda Castonguay <alcastonguay@gmail.com>
2022-01-21 18:37:47 -0500

    Add demo CORS-Anywhere fix for Medium RSS Feed.

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import Parser from 'rss-parser';
 import HomePerson1 from "../assets/images/home-person-1.svg";
 import HomePerson2 from "../assets/images/home-person-2.svg";
 import BackgroundVector from "../assets/images/background-vector.svg";
 import { Link } from 'react-router-dom'
 
 export default function homepage() {
+    
+    const parser = new Parser();
+    const [feed, setFeed] = React.useState([]);
+
+    React.useEffect(() => {
+        getFeed();
+    }, []);
+    
+    async function getFeed() {
+        try{
+            let feedInfo = [];
+            let thisFeed = await parser.parseURL('https://cors-anywhere.herokuapp.com/medium.com/feed/@kikiapp');
+            for(let i=0; i<3; i++){
+                let item = thisFeed.items[i];
+                let story = {
+                    title: item["title"],
+                    link: item["link"],
+                    image: item["content:encoded"].match(/<img([\s\S]+?)\/>/)[0]
+                }
+                feedInfo.push(story);
+            }
+            setFeed(feedInfo);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    
+    let blogPosts = feed.map(buildBlogPosts);
+
+    function buildBlogPosts(blogPost){
+        return ( 
+            <div key={blogPost.title} className="blog-post col-12 col-lg-4 p-3">
+                <a href={blogPost.link} target='_blank' rel="noreferrer">
+                <div dangerouslySetInnerHTML={{__html: blogPost.image}} />
+                <h3 className="text-center my-3">{blogPost.title}</h3> 
+                </a>
+            </div>
+        )
+    }
 
     return (
     <div className="homepage">
@@ -25,6 +65,12 @@ export default function homepage() {
                         <img className ="img-fluid" src={HomePerson2} alt="Person with blue pants and jacket sitting on a cube." />
                     </div>
                 </div>
+            </div>
+        </div>
+        <div className="row p-5 blog-post-container">
+            <div className="col">
+                <h3 className="text-center">Our Latest Posts...</h3>
+                <div className="row pt-5">{blogPosts}</div>
             </div>
         </div>
     </div>
