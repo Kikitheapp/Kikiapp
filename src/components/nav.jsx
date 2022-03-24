be9c989b814903126fa1a3f17005aa45c21513ac
Nick Fratt <nafratt@gmail.com>
2022-03-24 14:28:59 -0400

    aligned dorpdown with rest of nav(not-final)

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -22,13 +22,14 @@ export default function Navbar() {
         >
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className=' navbar-nav ml-auto'>
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul className=' navbar-nav ml-auto'>
+
           <li className='nav-item'>
           <div className="subnav">
           <button className="subnavbtn">Get Started <i className="fa fa-caret-down"></i></button>
           <div className="subnav-content">
-          <Link className='nav-link' to='/aboutus'>About Us</Link>
+          <Link className='nav-link nav-item' to='/aboutus'>About Us</Link>
           <Link className='nav-link' to='/faq'>FAQ</Link>
           <Link className='nav-link' to='/PrEP'>PrEP Q&amp;A</Link>
           <Link className='nav-link' to='/MeetTheSexToys'> Meet the sex toys</Link>
