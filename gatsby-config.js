9f92cc6907c50d6c01bdb536229d6a02143cbe3d
Amanda Castonguay <alcastonguay@gmail.com>
2022-05-26 23:15:25 -0400

    Fix version, add patreon page, fix Jill's picture, fix favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,23 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://kikitheapp.com/#/`,
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-sharp',
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: { 
+        name: `Kiki for the Future`,
+        short_name: `Kiki`,
+        start_url: `/`,
+        background_color: `#30C3CD`,
+        theme_color: `#30C3CD`,
+        display: `standalone`,
+        icon: 'src/assets/images/kikilogo1.png',
+        icons: [{src: `/favicons/android-chrome-32x32.png`, sizes: '32x32', type: 'image/png'}]
+      }
+    }
+  ]
 }
 
-module.exports = {
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-  ],
-}
