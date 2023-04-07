91b3787cc881116df5eb8658ccb9ad81a19468f9
Amanda Castonguay <alcastonguay@gmail.com>
2023-04-07 15:30:26 -0400

    Added Images and styles for Resources, display data in order by catagory, updated resources tests.

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,37 +2,67 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from '../layouts/layout/layout.js';
 import PageHeading from '../components/pageheading/page-heading.js';
-import Accordion from '../components/accordion/accordion.js';
 
-import person1 from "../assets/images/prep/prep-person-header-1.svg";
-import person2 from "../assets/images/prep/prep-person-header-2.svg";
+import person1 from "../assets/images/resources/resources-header-person-1.png";
+import person2 from "../assets/images/resources/resources-header-person-2.png";
 
 const pageInfo = {
   title: "Resources",
-  bgColor: "peach-puff",
+  bgColor: "light-orange",
   person1: person1,
   person2: person2
 }
 
+/**
+ * Resources page
+ * @param {Object} props.data - Resources data from DB 
+ * @returns {React.Component} Resources page component
+ **/
 function Resources(props){
 
-  let resources = props.data.allAirtable.edges;
-  
-  let resourceList = resources.map((resource, index) => {
-    return (
-      <Accordion title={resource.node.data.resource_name}>
-        <p>{resource.node.data.resource_type}</p>
-        <p>{resource.node.data.resource_states}</p>
-      </Accordion>
-    )
+  // get resources, grouped by type
+  const resources = props.data.resources.group;
+
+  // resource types in order that they are displayed
+  const resourceTypes = ['Housing', 'Legal', 'Health + Wellness', 'Queer Spaces'];
+  let resourceDivs = [];
+
+  // loop through the resource types and build the sections for the resource lists
+  resources.forEach((group) => {
+
+    // create a list of resources for this type
+    let resourceList = group.nodes.map((resource) => {
+      return (
+        <div className="resource ms-5">
+          <h4>{resource.data.resource_name}</h4>
+          <p>{resource.data.resource_description}<br />
+          Website: <a href={resource.data.resource_website}>{resource.data.resource_website}</a></p>
+        </div>
+      )
+    });
+
+    // create a section for this resource type and add the list of resources
+    let block = <div className="ms-5">
+    <h3>{group.type}</h3>
+      {resourceList}
+    </div>;
+
+    // add this section to the list of resource sections that will be rendered,
+    // in the order that they are displayed
+    resourceDivs[resourceTypes.indexOf(group.type)] = block;
+
   });
 
+  // TODO: Refactor resource sections into components
+  // TODO: Add filter by catagory (using dropdown, one catagory at a time)
+  
   return (
     <div>
       <Layout pageTitle={`${pageInfo.title} - Kiki for the Future`}>
         <PageHeading info={pageInfo}></PageHeading>
-        <div className="wrapper pb-5">
-          {resourceList}
+        <div className="container pb-5">
+          <h2>New York</h2>
+          {resourceDivs}
         </div>
       </Layout>
     </div>
@@ -41,17 +71,18 @@ function Resources(props){
 
 export const query = graphql`
   query {
-      allAirtable(filter: {table: {eq: "resources"}}) {
-        edges {
-          node {
-            data {
-              resource_name
-              resource_states
-              resource_type
-            }
+    resources: allAirtable(filter: {table: {eq: "resources"}}) {
+      group(field: data___resource_type) {
+        type: fieldValue
+        nodes {
+          data {
+            resource_name
+            resource_website
+            resource_description
           }
         }
       }
-    }`;
+    }
+  }`;
 
 export default Resources;
--- src/pages/resources.test.js
+++ src/pages/resources.test.js
@@ -6,12 +6,91 @@ import Resources from "./resources";
 describe("Resources", () => {
     
   it("renders", async () => {
+
+    const data = {
+        "resources": {
+          "group": [
+            {
+              "type": "Health + Wellness",
+              "nodes": [
+                {
+                  "data": {
+                    "resource_name": "Gay Men’s Health Crisis",
+                    "resource_description": "Gay Men’s Health Crisis",
+                    "resource_website": "https://www.gmhc.org/"
+                  }
+                },
+                {
+                  "data": {
+                    "resource_name": "Hispanic AIDS Forum",
+                    "resource_description": "HAF’s mission is to improve health outcomes for Latinos in New York City who are affected by HIV and other chronic illnesses. ",
+                    "resource_website": "http://hafnyc.org/"
+                  }
+                }
+              ]
+            },
+            {
+              "type": "Housing",
+              "nodes": [
+                {
+                  "data": {
+                    "resource_name": "Open Rainbow Residence",
+                    "resource_description": "Without any limits contributing to stopping homelessness in our community.",
+                    "resource_website": "https://www.facebook.com/openrainbowresidence/"
+                  }
+                }
+              ]
+            },
+            {
+              "type": "Legal",
+              "nodes": [
+                {
+                  "data": {
+                    "resource_name": "The HIV Law Project",
+                    "resource_description": "Serves low income NYC residents living with HIV/AIDS. Provides help with legal issues involving: public benefits, housing, or immigration.\n",
+                    "resource_website": "https://www.housingworks.org/legal/hiv-law-project"
+                  }
+                }
+              ]
+            },
+            {
+              "type": "Queer Spaces",
+              "nodes": [
+                {
+                  "data": {
+                    "resource_name": "The Center",
+                    "resource_description": "The Center is the cornerstone of our LGBTQ community in New York City. \nEach year, more than 400 community groups come here to meet and make \nconnections, find support systems, and to take advantage of our many \nresources.",
+                    "resource_website": "https://gaycenter.org/"
+                  }
+                },
+                {
+                  "data": {
+                    "resource_name": "Growing LGBTQ+ Youth Support - Western New York",
+                    "resource_description": "GLYS provides a safe and positive environment for LGBTQ+ youth to learn more about themselves through peer interaction and educational experiences. This chapter serves Western NY.\n\n",
+                    "resource_website": "https://www.glyswny.org/"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+      "extensions": {}
+    };
     
-    // TODO: Mock data so test works.
-    //render(<Resources />);
-    
+    render(<Resources data={data} />);
+
     // Find heading, throw error if not found.
-    //const heading = await screen.findByText("Resources");
+    // Ignore navbar links. 
+    const heading = await screen.findByText("Resources", {ignore: "script, style, nav a"});
+
+    // Find state heading, throw error if not found.
+    const state = await screen.findByText("New York");
+
+    // Find sections, throw error if not found.
+    const housing = await screen.findByText("Housing");
+    const legal = await screen.findByText("Legal");
+    const health = await screen.findByText("Health + Wellness");
+    const spaces = await screen.findByText("Queer Spaces");
 
   });
     
