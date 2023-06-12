4ce8bf2af8a096e53fc05eace968b4026f48141c
Amanda Castonguay <alcastonguay@gmail.com>
2023-06-12 18:10:17 -0400

    Fix SSR error

--- src/components/escape/escape.js
+++ src/components/escape/escape.js
@@ -1,38 +1,38 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import './escape.css';
 import { BsFillXCircleFill } from 'react-icons/bs';
 
 
+function Escape(props) {
 
-// catch escape button press
-document.addEventListener('keydown', exitButton);
+  // catch escape button press
+  useEffect(() => {
+    document.addEventListener('keydown', exitButton);
+  });
 
-let keyCount = 0;
-function exitButton(event) {
+  let keyCount = 0;
+  function exitButton(event) {
 
-  event.preventDefault();
+    event.preventDefault();
 
-  console.log(event);
-  console.log(keyCount);
-  if(event.key === 'Escape') {
-    keyCount++;
-    if (keyCount === 3) {
-      window.location.replace("https://google.com");
+    if(event.key === 'Escape') {
+      keyCount++;
+      if (keyCount === 3) {
+        window.location.replace("https://google.com");
+      }
+
+      setTimeout(() => {
+        keyCount = 0;
+      }, 2000);
     }
-
-    setTimeout(() => {
-      keyCount = 0;
-    }, 2000);
   }
 
-}
+  function exitClick (event) {
+    event.preventDefault();
+    window.location.replace("https://google.com");
+  }
 
-function exitClick (event) {
-  event.preventDefault();
-  window.location.replace("https://google.com");
-}
-
-function Escape(props) {
   return (
     <div className="d-inline-block sticky-top start-50 m-2 escape">
       <a href="https://google.com" className="btn btn-escape bg-peach-puff" onClick={exitClick}>
