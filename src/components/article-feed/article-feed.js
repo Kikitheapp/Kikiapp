b5e1baeab8cbb251e55a32baf5060370d41981b4
Amanda Castonguay <alcastonguay@gmail.com>
2023-06-29 16:43:38 -0400

    Make article cards.

--- src/components/article-feed/article-feed.js
+++ src/components/article-feed/article-feed.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-//import './article-feed.css';
+import './article-feed.css';
 
 /**
  * ArticleFeed component - get Kiki articles from Medium and display the three most recent.
@@ -30,13 +30,42 @@ function ArticleFeed() {
     }
   `);
 
-  console.log(data);
+  let articles;
+  try {
+    articles = data.allMediumPost.edges;
+  } catch (error) {
+    console.error(error);
+  } 
 
+  
+  let articleFeed = '';
+  if(articles && articles.length > 0){
+    // build article cards
+
+    let articleCards = articles.map(buildArticleCard);
+
+    articleFeed = <div className="row justify-content-center p-5 article-feed bg-dark-orange text-center"><div className="row"><h2>Our latest posts...</h2></div><div className="row">{articleCards}</div></div>;
+  }
+
+  function buildArticleCard(article){
+    return (
+      <div className="col-8 my-3 col-lg-4" key={article.node.id}>
+        <div className="card">
+        <a href={`https://medium.com/kiki-magazine/${article.node.uniqueSlug}`} target="_blank" rel="noreferrer">
+          <img className="card-img-top" src={`https://miro.medium.com/max/1000/${article.node.virtuals.previewImage.imageId}`} alt={article.node.title} />
+          <div className="card-body">
+            <h4 className="card-title">{article.node.title}</h4>
+          </div>
+        </a>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="article-feed">
-      <h1>Our latest posts...</h1>
-    </div>
+    <>
+    {articleFeed}
+    </>
   );
 }
 
